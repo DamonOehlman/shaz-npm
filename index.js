e658f7e335e0904a409d33abcba6e79eb6039185
Damon Oehlman <damon.oehlman@gmail.com>
2014-09-14 13:58:36 +0300

    It works...

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 var crel = require('crel');
+var insertcss = require('insert-css');
+var fs = require('fs');
+
+/**
+  # shaz-npm
+
+  A [shaz](https://github.com/DamonOehlman/shaz) plugin for calling out npm
+  packages in your slides.
+
+  ## Example Usage
+
+  <<< examples/simple.js
+**/
 
 module.exports = function(Slide, opts) {
+  insertcss((opts || {}).css || fs.readFileSync(__dirname + '/style.css'));
+
   Slide.prototype.npm = function(pkgName) {
-    this.el.appendChild(crel('code', pkgName));
+    this.svg(fs.readFileSync(__dirname + '/images/npm-logo.svg'));
+    this.bgSize('20%');
+
+    this.el.classList.add('npm');
+    this.el.appendChild(
+      crel('div', { class: 'npm-package' },
+        crel('a', { href: 'https://npmjs.org/' + pkgName },
+          crel('code', pkgName)
+        )
+      )
+    );
 
     return this;
   };
